ac345bdbac4d6b67f99de6980f8a893755d93c5b
MetaSwapper <metaswap.info@gmail.com>
2025-10-01 02:55:13 +0200

    Fix bug

--- src/components/SwapForm.tsx
+++ src/components/SwapForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChangeEvent, useCallback, useEffect, useMemo, useState } from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import {
   LAMPORTS_PER_SOL,
@@ -59,6 +59,14 @@ const DEFAULT_USDC_TOKEN: TokenInfo = {
 
 const decodeTransaction = (encoded: string) =>
   Uint8Array.from(atob(encoded), (character) => character.charCodeAt(0));
+
+const WalletMultiButton = dynamic(
+  () =>
+    import("@solana/wallet-adapter-react-ui").then(
+      (mod) => mod.WalletMultiButton
+    ),
+  { ssr: false }
+);
 
 const SwapForm = () => {
   const { network, setNetwork } = useNetwork();
